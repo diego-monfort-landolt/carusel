946cac068d99dacb87aeef3a9e7f1a5c7770a8d3
Diego19890102 <diegomonfort1989@gmail.com>
2022-12-20 19:30:07 +0100

    update main.js
    geändert weil der code so nicht funktioniert hat -translateX( ${ operacion }%) to grande.style.transform = 'translateX( -50%)'
            grande.style.transform = 'translateX( -50%)'

--- main.js
+++ main.js
@@ -7,17 +7,19 @@ punto.forEach( ( cadaPunto , i) => {
     punto[i].addEventListener('click', ()=> {
 
         let posicion = i
-        let operacion = posicion * -50
-
-        grande.style.transform = 'translateX( ${operacion} %)'
+       // let operacion = posicion * -50
+//geändert weil der code so nicht funktioniert hat -translateX( ${ operacion }%) to grande.style.transform = 'translateX( -50%)'
+        grande.style.transform = 'translateX( -50%)'
         punto.forEach((cadaPunto, i)=>{
             
             punto[i].classList.remove('activo')
 
         })
-        punto[i].classList.add('activo')
+            punto[i].classList.add('activo')
 
 
     })
-})
+});
+
+
 
